3ab7fc4d86bc3da70ffb73be9e2a0f6a0e922456
sudipto <cg.sudipc@gmail.com>
2023-08-05 22:50:26 +0530

    c1

---
+++ src/main/java/com/example/testclasses/MathUtils.java
@@ -0,0 +1,10 @@
+package com.example.testclasses;
+
+public class MathUtils {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	
+}
---
+++ src/test/java/com/example/testclasses/MathUtilsTest.java
@@ -0,0 +1,17 @@
+package com.example.testclasses;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		MathUtils mathUtils = new MathUtils();
+		int expected = 1;
+		int actual = mathUtils.add(1, 1);
+		assertEquals(expected, actual);
+	}
+
+}
